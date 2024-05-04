86b279688a937a521a3fa28019ac52715b7f26c7
Fuad J Torres <fuadjtorres99@gmail.com>
2024-05-03 21:35:55 -0500

    i did not do this right

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,17 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = `This was last modifide: ${new Date(document.lastModified)}`;
+
+
+const button = document.getElementById("button1")
+const links = document.querySelectorAll(".link")
+
+button.addEventListener("click", () => {
+
+    //alert(links.length)
+    
+    if (button.textContent == "X") {button.textContent = "≡";}
+    else {button.textContent = "X";}
+
+    links.forEach((i) => {i.classList.toggle('men');});
+    
+})
