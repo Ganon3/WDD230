5d026833dcd6c25d2ac5c82bcf40f3ef11b26e63
Fuad J Torres <fuadjtorres99@gmail.com>
2024-05-25 17:21:51 -0600

    Finaly

--- scripts/links.js
+++ scripts/links.js
@@ -4,44 +4,45 @@ const linksURL = "https://ganon3.github.io/WDD230/data/links.json";
 async function Main(link) { try { // this is the main funtion to work with the Json
     
                      const rawData = await fetch(link);  
-    if(rawData.ok) { const data = await rawData.json();       
+    if(rawData.ok) { const data = await rawData.json();       // console.log(data);   
         
-    //console.table(data); 
-    //console.table(data.weeks);
-    //console.log(data.weeks.week2)
-    console.log(data);
-    displayTest(data);
-//    display(data.weeks);
+    // displayTest(data.weeks);
+    display(data.weeks);
         
 } else {throw Error ("There was a probelm with the links.js")}
 } catch (err) {alert(err)}}
 
+// testing stuff
+// testing  --------
 function displayTest (array) {
-    array.forEach(i => {console.log(i)});
-    array.weeks.forEach(i => console.log(i));
+
+    array.forEach(i => console.log(i));
+    array.forEach(i => console.log("hi"));
+    array.forEach(i => console.log(i.week));
+    array.forEach(i => console.log(i.links));
+    array.forEach(i => console.log(i.links[0]));
 }
 
-
-// funtion display 
+// funtion display
+// display ---------
 const links1 = document.getElementById("links");
 
 function display (array) { array.forEach(week => {
     
     const li = document.createElement("li");
     const div = document.createElement("div");
+    const h3 = document.createElement("h3");
 
-    week.forEach(i => {
-        if (week[0] == i) {
-            const h3 = document.createElement("h3");
-            h3.innerHTML = i.name;
+            h3.innerHTML = week.week;
             li.appendChild(h3);
-        }
-        else {
-            const a = document.createElement("a");
-            a.href = i.url;
-            a.innerHTML = i.title
-            div.appendChild(a);
-        }
+
+    week.links.forEach(i => {
+
+        const a = document.createElement("a");
+        a.href = i.url;
+        a.innerHTML = i.title
+        div.appendChild(a);
+
     })
 
     li.appendChild(div);
