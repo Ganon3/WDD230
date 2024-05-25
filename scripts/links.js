5bd152e374fd818c80eda985f0f6767e4fcc8146
Fuad J Torres <fuadjtorres99@gmail.com>
2024-05-25 16:03:40 -0600

    fixing stuff

--- scripts/links.js
+++ scripts/links.js
@@ -1,18 +1,46 @@
 const baseURL = "https://ganon3.github.io/WDD230/";
 const linksURL = "https://ganon3.github.io/WDD230/data/links.json";
 
-
-
 async function Main(link) { try {                             // this is the main funtion to work with the Json
     
                      const rawData = await fetch(link);       // this is the relm of tests
-    if(rawData.ok) { const data = await rawData.json();       console.table(data); 
-
-
-
-
+    if(rawData.ok) { const data = await rawData.json();       
+        
+    console.table(data); 
+    console.table(data.weeks);
+    console.log(data.weeks || "hellow")
+    display(data);
+        
 } else {throw Error ("There was a probelm with the links.js")}
-} catch (err) {alert(err)}
-}
+} catch (err) {alert(err)}}
 
+
+// funtion display 
+const links1 = document.getElementById("links");
+
+function display (data) { data.weeks.forEach(week => {
+    
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+
+    week.forEach(i => {
+        if (week[0] == i) {
+            const h3 = document.createElement("h3");
+            h3.innerHTML = i.name;
+            li.appendChild(h3);
+        }
+        else {
+            const a = document.createElement("a");
+            a.href = i.url;
+            a.innerHTML = i.title
+            div.appendChild(a);
+        }
+    })
+
+    li.appendChild(div);
+    links1.appendChild(li);
+
+});}
+
+// to activate the program
 Main(linksURL);
