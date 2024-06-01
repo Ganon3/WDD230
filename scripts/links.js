ce36da1e51231ae8a53335e677746a8ffe4affed
Fuad J Torres <fuadjtorres99@gmail.com>
2024-06-01 15:35:44 -0500

    Fixed that problem

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
-const baseURL = "https://ganon3.github.io/WDD230/";
-const linksURL = "https://ganon3.github.io/WDD230/data/links.json";
+const baseURL = "https://ganon3.github.io/wdd230/";
+const linksURL = "https://ganon3.github.io/wdd230/data/links.json";
 
 async function Main(link) { try { // this is the main funtion to work with the Json
     
